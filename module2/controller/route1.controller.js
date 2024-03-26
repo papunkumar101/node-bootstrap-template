41b3954aa29ed23bd128a0d90d7e7ff6b37f7766
papun kumar <papunkumar101>
2024-03-26 22:27:34 +0530

    chnage the route hello world to api status

--- module2/controller/route1.controller.js
+++ module2/controller/route1.controller.js
@@ -2,11 +2,11 @@ import Route1Service from '../service/route1.service.js';
 
 
 class Route1Controller{
-    async heyHello(req, res, next) {
+    async apiStatus(req, res, next) {
         /* 	#swagger.tags = ['Open']
                         #swagger.description = 'Hello World ' */
                         
-        return await Route1Service.heyHello(req, res, next);
+        return await Route1Service.apiStatus(req, res, next);
       }
 
 }
